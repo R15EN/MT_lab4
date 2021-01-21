f259cea3a1aafb7142bd302f4024dc71f2f70e30
R15EN <48097902+R15EN@users.noreply.github.com>
2021-01-21 20:51:07 +0300

    lab4

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    public Transform Begin;
+    public Transform End;
+    
+    private Vector3 direction;
+    public bool visible = false;
+    void Start()
+    {
+       
+        direction = new Vector3(0, 0, -1);
+    }
+
+    void Update()
+    {
+        transform.Translate(direction* 5* Time.deltaTime);
+        
+    }
+}
